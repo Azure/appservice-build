05c55232f080a24b29c833b34cc751a2d6966741
Stella Qian <zqian@microsoft.com>
2020-02-18 17:52:02 -0800

    add startTime and endTime as env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,4 +40,14 @@ fi
 
 echo
 echo "Running command '${oryxCommand}'"
+
+url="https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs"
+
+#Format: "2020-02-15T02:51:50.000Z"
+startTime=$(curl -X GET "${url}" | sed 's/,/\n/g' | grep "started_at" | awk '{print $2}' | sed -n '3p')
+endTime=$(curl -X GET "${url}" | sed 's/,/\n/g' | grep "completed_at" | awk '{print $2}' | sed -n '3p')
+
+export GITHUB_BUILD_CONTAINER_START=$startTime
+export GITHUB_BUILD_CONTAINER_COMPLETE=$endTime
+
 eval $oryxCommand
