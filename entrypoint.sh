853c0aa8cbb7f10e0cb057a1d1deaf856249abac
Stella Qian <zqian@microsoft.com>
2020-02-26 13:16:55 -0800

    for test new image

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,4 +52,5 @@ if [ -z "$ORYX_DISABLE_TELEMETRY" ] || [ "$ORYX_DISABLE_TELEMETRY" == "false" ];
     export GITHUB_ACTIONS_BUILD_IMAGE_PULL_END_TIME=$endTime
 fi
 
+export ORYX_SDK_STORAGE_BASE_URL="https://oryxsdksdev.blob.core.windows.net"
 eval $oryxCommand
