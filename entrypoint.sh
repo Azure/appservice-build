6ac712a908529cc32698ec6a051ba49ac49a7df1
Kiran Challa <kichalla@microsoft.com>
2020-03-11 16:12:03 -0700

    Updated entrypoint script to accept output directory as an argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,41 +1,82 @@
-#!/bin/sh -l
+#!/bin/bash
+set -ex
 
-sourceDirectory=$1
-platform=$2
-platformVersion=$3
+checkArg() {
+    # Check if the input has content and is not just whitepsace or is empty
+    case $1 in
+    (*[![:blank:]]*) echo "$1";;
+    (*) echo ""
+    esac
+}
+
+getAbsolutePath(){
+    local path="$1"
+
+    if [ -z "$path" ]; then
+        echo "$PWD"
+    fi
+
+    # Check if the path is relative or absolute.
+    # If relative path is given, create an absolute path
+    case $path in
+    /*) echo $path ;;
+    *) echo "$PWD/$path" ;;
+    esac
+}
+
+sourceDirectory=$(checkArg "$1")
+platform=$(checkArg "$2")
+platformVersion=$(checkArg "$3")
+outputDirectory=$(checkArg "$4")
 
 echo
-
-if [ -n "${sourceDirectory}" ]
+if [ -z "$sourceDirectory" ]
 then
-    sourceDirectory="$PWD/$sourceDirectory"
-    echo "Relative path to source directory provided -- the following directory will be built: '${sourceDirectory}'"
+    sourceDirectory="$PWD"
+    echo "No source directory was provided -- the root of the repository ('GITHUB_WORKSPACE' environment variable) will be built: '${sourceDirectory}'"
 else
-    sourceDirectory=$PWD
-    echo "No source directory provided -- the root of the repository ('GITHUB_WORKSPACE' environment variable) will be built: '${sourceDirectory}'"
+    sourceDirectory=$(getAbsolutePath "$sourceDirectory")
+    echo "Relative path to source directory was provided -- the following directory will be built: '${sourceDirectory}'"
 fi
 
 echo
-oryxCommand="oryx build ${sourceDirectory}"
+oryxCommand="oryx build $sourceDirectory"
+
+if [ ! -z "$outputDirectory" ]
+then
+    outputDirectory=$(getAbsolutePath "$outputDirectory")
+    oryxCommand="$oryxCommand -o $outputDirectory"
+fi
 
 echo
-
-if [ -n "${platform}" ]
+if [ -z "$platform" ]
 then
-    echo "Platform provided: '${platform}'"
-    oryxCommand="${oryxCommand} --platform ${platform}"
-else
     echo "No platform provided -- Oryx will enumerate the source directory to determine the platform."
+else
+    echo "Platform provided: '$platform'"
+    oryxCommand="$oryxCommand --platform $platform"
 fi
 
 echo
-
-if [ -n "${platformVersion}" ]
+if [ -z "$platformVersion" ]
 then
-    echo "Platform version provided: '${platformVersion}'"
-    oryxCommand="${oryxCommand} --platform-version ${platformVersion}"
-else
     echo "No platform version provided -- Oryx will determine the version."
+else
+    echo "Platform version provided: '$platformVersion'"
+    oryxCommand="$oryxCommand --platform-version $platformVersion"
+fi
+
+oryxCommand="${oryxCommand} --enable-dynamic-install"
+
+if [ -z "$ORYX_DISABLE_TELEMETRY" ] || [ "$ORYX_DISABLE_TELEMETRY" == "false" ]; then
+    url="https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs"
+    json=$(curl -X GET "${url}")
+    startTime=${json#*Build*/appservice-build@*,}
+    startTime=$(echo "${startTime}"| sed 's/,/\n/g' | grep "started_at" | awk '{print $2}' | sed -n '1p')
+    endTime=${json#*Build*/appservice-build@*,}
+    endTime=$(echo "${endTime}" | sed 's/,/\n/g' | grep "completed_at" | awk '{print $2}' | sed -n '1p')
+    export GITHUB_ACTIONS_BUILD_IMAGE_PULL_START_TIME=$startTime
+    export GITHUB_ACTIONS_BUILD_IMAGE_PULL_END_TIME=$endTime
 fi
 
 echo
