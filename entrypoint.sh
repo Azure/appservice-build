a1dcd977ce906077ea4b01e7fce1e7354fb39fdd
Kiran <kichalla@users.noreply.github.com>
2020-03-12 15:17:37 -0700

    Updated entrypoint script to accept output directory as an argument (#12)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,46 +1,72 @@
-#!/bin/sh -l
+#!/bin/bash
+set -e
 
-sourceDirectory=$1
-platform=$2
-platformVersion=$3
+checkArg() {
+    # Check if the input has content and is not just whitepsace or is empty
+    case $1 in
+        (*[![:blank:]]*) echo "$1";;
+        (*) echo ""
+    esac
+}
+
+getAbsolutePath(){
+    local path="$1"
+
+    if [ -z "$path" ]; then
+        echo "$PWD"
+    fi
+
+    # Check if the path is relative or absolute.
+    # If relative path is given, create an absolute path
+    case $path in
+        /*) echo $path ;;
+        *) echo "$PWD/$path" ;;
+    esac
+}
+
+sourceDirectory=$(checkArg "$1")
+platform=$(checkArg "$2")
+platformVersion=$(checkArg "$3")
+outputDirectory=$(checkArg "$4")
 
 echo
-
-if [ -n "${sourceDirectory}" ]
+if [ -z "$sourceDirectory" ]
 then
-    sourceDirectory="$PWD/$sourceDirectory"
-    echo "Relative path to source directory provided -- the following directory will be built: '${sourceDirectory}'"
+    sourceDirectory="$PWD"
+    echo "No source directory was provided -- the root of the repository ('GITHUB_WORKSPACE' environment variable) will be built: '$sourceDirectory'"
 else
-    sourceDirectory=$PWD
-    echo "No source directory provided -- the root of the repository ('GITHUB_WORKSPACE' environment variable) will be built: '${sourceDirectory}'"
+    sourceDirectory=$(getAbsolutePath "$sourceDirectory")
+    echo "Source directory was provided. Following directory will be built: '$sourceDirectory'"
 fi
 
-echo
-oryxCommand="oryx build ${sourceDirectory}"
+oryxCommand="oryx build $sourceDirectory"
 
-echo
-
-if [ -n "${platform}" ]
+if [ -z "$outputDirectory" ]
 then
-    echo "Platform provided: '${platform}'"
-    oryxCommand="${oryxCommand} --platform ${platform}"
+    echo "No output directory was provided"
 else
+    outputDirectory=$(getAbsolutePath "$outputDirectory")
+    oryxCommand="$oryxCommand -o $outputDirectory"
+    echo "Output directory was provided: '$outputDirectory'"
+fi
+
+if [ -z "$platform" ]
+then
     echo "No platform provided -- Oryx will enumerate the source directory to determine the platform."
+else
+    echo "Platform provided: '$platform'"
+    oryxCommand="$oryxCommand --platform $platform"
 fi
 
-echo
-
-if [ -n "${platformVersion}" ]
+if [ -z "$platformVersion" ]
 then
-    echo "Platform version provided: '${platformVersion}'"
-    oryxCommand="${oryxCommand} --platform-version ${platformVersion}"
-else
     echo "No platform version provided -- Oryx will determine the version."
+else
+    echo "Platform version provided: '$platformVersion'"
+    oryxCommand="$oryxCommand --platform-version $platformVersion"
 fi
 
 oryxCommand="${oryxCommand} --enable-dynamic-install"
-echo
-echo "Running command '${oryxCommand}'"
 
 if [ -z "$ORYX_DISABLE_TELEMETRY" ] || [ "$ORYX_DISABLE_TELEMETRY" == "false" ]; then
     url="https://api.github.com/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/jobs"
@@ -55,4 +81,6 @@ fi
 
 export ORYX_SDK_STORAGE_BASE_URL="https://oryxsdk-cdn.azureedge.net"
 
+echo
+echo "Running command '$oryxCommand'"
 eval $oryxCommand
